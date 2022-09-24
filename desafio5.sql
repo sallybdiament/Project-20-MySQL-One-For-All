9d32803c0bd5e24ad7530c796d55ead98c9c4ebe
Sally Diament <sally.barzilai@gmail.com>
2022-09-23 23:57:17 -0300

    corrigindo o requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
 SELECT songs.song_name AS cancao, COUNT(history.user_id) AS reproducoes
 FROM SpotifyClone.history 
 INNER JOIN SpotifyClone.songs ON SpotifyClone.history.song_id = SpotifyClone.songs.song_id 
-GROUP BY cancao ORDER BY reproducoes DESC LIMIT 2;
+GROUP BY cancao ORDER BY reproducoes DESC, cancao ASC LIMIT 2 ;
